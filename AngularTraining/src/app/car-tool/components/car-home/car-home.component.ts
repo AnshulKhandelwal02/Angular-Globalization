46b8cebec4095b2eb3d012c43a02e534c60cc808
Anshul Khandelwal <Anshul.Khandelwal@EYC.COM>
2017-12-08 16:49:29 +0530

    Exercise 2 complete

--- AngularTraining/src/app/car-tool/components/car-home/car-home.component.ts
+++ AngularTraining/src/app/car-tool/components/car-home/car-home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Car } from '../../models/car';
+import { HttpClientModule } from '@angular/common/http/src/module';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,9 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarHomeComponent implements OnInit {
 
-  constructor() { }
+  cars: Car[];
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+
+    this.httpClient.get<Car[]>('http://localhost:3050/cars')
+    .subscribe(cars => this.cars = cars);
+
   }
 
 }
